016916f0fe5114a786e1d3b6a08213847b67e6ed
gauravn17 <93863977+gauravn17@users.noreply.github.com>
2025-05-04 02:20:30 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './Signup';
 import MatchPage from './MatchPage';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <div className="min-h-screen bg-gray-100 p-4">
         <Routes>
           <Route path="/" element={<MatchPage />} />
